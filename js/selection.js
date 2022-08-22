ed936a98e264675be7a6ce4ea56c06830a56cf20
delower68 <hdelower68@gmail.com>
2022-08-23 02:18:26 +0800

    player name show done

---
+++ js/selection.js
@@ -0,0 +1,41 @@
+const playerArry = [];
+
+function displayName(){
+    let totalName = 0 ;
+
+    const totalNames = document.getElementById('totalPlayerName');
+    totalNames.innerText = playerArry.length ;
+
+    const topPlayerName = document.getElementById('eachPlayerName');
+    topPlayerName.textContent = '' ;
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <th></th>
+    <td>Player Name</td>
+    <td>${totalName}</td>
+    `;
+
+    topPlayerName.appendChild(tr);
+    for( let i = 0; i < playerArry.length; i++ )
+    {
+        totalName += playerArry[i].playerName ;
+
+        const tr = document.createElement("tr")
+        tr.innerHTML = `
+        <th>${i+1}</th>
+        <td>${playerArry[i].playerName}</td>
+        `;
+        topPlayerName.appendChild(tr);
+    }
+    
+}
+function addToSelection(element){
+    const playerName = element.parentNode.parentNode.children[0].innerText ;
+    const PlayerNameObj = {
+        playerName: playerName
+    }
+    playerArry.push(PlayerNameObj) ;
+    displayName();
+}
+
