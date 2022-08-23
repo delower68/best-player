6d92ce4f4aecefb9bb32546b9ca5a852f8cd3668
delower68 <hdelower68@gmail.com>
2022-08-23 15:20:42 +0800

    common function used

--- js/budget.js
+++ js/budget.js
@@ -1,16 +1,22 @@
-document.getElementById('totalPlayerCost').addEventListener('click', function(){
-    const perPlayerCost = document.getElementById('perPlayerInputField');
-    const totalperPlayerInputFieldString = perPlayerCost.value ;
-    const  totalperPlayerInputField = parseFloat(totalperPlayerInputFieldString);
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId)
+    const inputFieldValueStr = inputField.value
+    const inputFieldValue = parseFloat(inputFieldValueStr)
+    inputField.value = ''
+    return inputFieldValue
+}
 
+function getTextElementValueById(elementId) {
+    const textElement = document.getElementById(elementId)
+    const textElementValue = textElement.innerText
+    return textElementValue
+}
+document.getElementById('totalPlayerCost').addEventListener('click', function(){
+    const totalperPlayerInputField = getInputFieldValueById('perPlayerInputField');
     // get selected player number 
 
-    const playerNumber = document.getElementById('totalPlayerName');
-    const playerNumberString = playerNumber.innerText ;
-    const totalPlayerNumber = parseFloat(playerNumberString); 
-    totalPlayerNumber.innerText = totalperPlayerInputField
-    perPlayerCost.value = '' ;
-    
+    const totalPlayerNumber = getTextElementValueById('totalPlayerName')
+        
     // get the value of total player cost calculate 
     const myTotalPlayerCost = totalPlayerNumber * totalperPlayerInputField ;
 
@@ -46,3 +52,52 @@ document.getElementById('totalCostCalculate').addEventListener('click', function
     const myAllCostPlayers = managerInputField + coachInput ;
     console.log(myAllCostPlayers);
 })
+
+// document.getElementById('totalPlayerCost').addEventListener('click', function(){
+//     const perPlayerCost = document.getElementById('perPlayerInputField');
+//     const totalperPlayerInputFieldString = perPlayerCost.value ;
+//     const  totalperPlayerInputField = parseFloat(totalperPlayerInputFieldString);
+
+//     // get selected player number 
+
+//     const playerNumber = document.getElementById('totalPlayerName');
+//     const playerNumberString = playerNumber.innerText ;
+//     const totalPlayerNumber = parseFloat(playerNumberString); 
+//     totalPlayerNumber.innerText = totalperPlayerInputField
+//     perPlayerCost.value = '' ;
+    
+//     // get the value of total player cost calculate 
+//     const myTotalPlayerCost = totalPlayerNumber * totalperPlayerInputField ;
+
+//     const myTotalPlayervalues = document.getElementById('totalPlayervalue');
+//     const myTotalPlayervaluesString = myTotalPlayervalues.innerText ;
+//     const myTotalPlayervalue = parseFloat(myTotalPlayervaluesString);
+    
+//     myTotalPlayervalues.innerText = myTotalPlayerCost ;
+
+
+// })
+
+//     // get the total cost of all players 
+// document.getElementById('totalCostCalculate').addEventListener('click', function()
+// {
+//     // get the mamager field value 
+//     const managerInputs = document.getElementById('managerInput');
+//     const managerInputFieldString = managerInputs.value ;
+//     const managerInputField = parseFloat(managerInputFieldString);
+//     managerInputs.value = '' ;
+//     // console.log(managerInputField);
+
+//     // get the coach field value 
+
+//     const coachInputs = document.getElementById('coachInput');
+//     const coachInputString = coachInputs.value ;
+//     const coachInput = parseFloat(coachInputString);
+//     coachInputs.value = '' ;
+//     // console.log(coachInput);
+
+//     // get the all cost of players
+
+//     const myAllCostPlayers = managerInputField + coachInput ;
+//     console.log(myAllCostPlayers);
+// })
--- js/selection.js
+++ js/selection.js
@@ -3,6 +3,9 @@ const playerArry = [];
 function displayName(){
     let totalName = 0 ;
 
+    // let index = 1
+    // let tablelength = document.getElementById('tableData').childNodes.index[] ;
+    // console.log(tablelength);
     const totalNames = document.getElementById('totalPlayerName');
     totalNames.innerText = playerArry.length ;
 
@@ -19,7 +22,8 @@ function displayName(){
     topPlayerName.appendChild(tr);
     for( let i = 0; i < playerArry.length; i++ )
     {
-        totalName += playerArry[i].playerName ;
+        if (i < 5) {
+            totalName += playerArry[i].playerName ;
 
         const tr = document.createElement("tr")
         tr.innerHTML = `
@@ -27,6 +31,11 @@ function displayName(){
         <td>${playerArry[i].playerName}</td>
         `;
         topPlayerName.appendChild(tr);
+        }
+        else{
+            alert('sor bari jha ')
+        }
+        
     }
     
 }
@@ -41,5 +50,56 @@ function addToSelection(element){
 
 
 // player name shows done 
+
+
+// const playerArry = [];
+
+// function displayName(){
+//     let totalName = 0 ;
+
+//     const totalNames = document.getElementById('totalPlayerName');
+//     totalNames.innerText = playerArry.length ;
+
+//     const topPlayerName = document.getElementById('eachPlayerName');
+//     topPlayerName.textContent = '' ;
+
+//     const tr = document.createElement('tr');
+//     tr.innerHTML = `
+//     <th></th>
+//     <td>Player Name</td>
+//     <td>${totalName}</td>
+//     `;
+
+//     topPlayerName.appendChild(tr);
+//     for( let i = 0; i < playerArry.length; i++ )
+//     {
+//         if (i < 5) {
+//             totalName += playerArry[i].playerName ;
+
+//         const tr = document.createElement("tr")
+//         tr.innerHTML = `
+//         <th>${i+1}</th>
+//         <td>${playerArry[i].playerName}</td>
+//         `;
+//         topPlayerName.appendChild(tr);
+//         }
+//         else{
+//             alert('sor bari jha ')
+//         }
+        
+//     }
+    
+// }
+// function addToSelection(element){
+//     const playerName = element.parentNode.parentNode.children[0].innerText ;
+//     const PlayerNameObj = {
+//         playerName: playerName
+//     }
+//     playerArry.push(PlayerNameObj) ;
+//     displayName();
+// }
+
+
+// // player name shows done 
 
 
